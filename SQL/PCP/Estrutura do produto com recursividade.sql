9b26eb03f7a2627a3974958eeb558266b1a090b8
Mateus Viccari <mateus.viccari@migplus.com.br>
2025-10-04 10:26:06 -0300

    fix: change db names

--- SQL/PCP/Estrutura do produto com recursividade.sql
+++ SQL/PCP/Estrutura do produto com recursividade.sql
@@ -10,7 +10,7 @@ select R.B1_COD, R.B1_DESC, estr.*
 from SB1010 as R with(nolock)
 cross apply (
 	select codigo, quantidade, perda, codigo_superior, nivel, intermediario, ordem_hierarquica
-	from TIMIGPLUS.estoque.estrutura_com_recursividade(R.B1_COD, R.B1_QB, 0, 1)
+	from MY_DB.estoque.estrutura_com_recursividade(R.B1_COD, R.B1_QB, 0, 1)
 ) as estr
 inner join SB1010 as I with(nolock) on I.D_E_L_E_T_ = '' and I.B1_FILIAL = '00' and I.B1_COD = estr.codigo collate latin1_general_bin
 where R.D_E_L_E_T_ = '' and R.B1_FILIAL = '00'
@@ -49,12 +49,12 @@ begin
 	@nivel + 1 as nivel,
 	intermediario.eh,
 	trim(@codigo_produto) + ' > ' + trim(G1_COMP)
-	from DADOSADV.dbo.SG1010 as G1 with(nolock)
-	inner join DADOSADV.dbo.SB1010 as B1R with(nolock) on B1R.D_E_L_E_T_ = '' and B1R.B1_FILIAL = '00' and B1R.B1_COD = G1.G1_COD
+	from PROTHEUS_DB.dbo.SG1010 as G1 with(nolock)
+	inner join PROTHEUS_DB.dbo.SB1010 as B1R with(nolock) on B1R.D_E_L_E_T_ = '' and B1R.B1_FILIAL = '00' and B1R.B1_COD = G1.G1_COD
 	cross apply (select case when B1R.B1_QB = 0 then 1 else B1R.B1_QB end as qtd_base) as produto_pai
 	cross apply (
 		select case when exists
-			(select 1 from DADOSADV.dbo.SG1010 as G1I with(nolock)
+			(select 1 from PROTHEUS_DB.dbo.SG1010 as G1I with(nolock)
 			where G1I.D_E_L_E_T_ = '' and G1I.G1_FILIAL = '00' and G1I.G1_COD = G1.G1_COMP)
 			then 1 else 0 end
 	) as intermediario(eh)
@@ -69,15 +69,15 @@ begin
 	insert into @retorno(codigo_superior, codigo, quantidade, perda, nivel, intermediario, ordem_hierarquica)
 	SELECT
 	codigo_superior, codigo, quantidade, perda, nivel + 1, intermediario, trim(@codigo_produto) + ' > ' + ordem_hierarquica
-	from DADOSADV.dbo.SG1010 as G1 with(nolock)
-	inner join DADOSADV.dbo.SB1010 as B1R with(nolock) on B1R.D_E_L_E_T_ = '' and B1R.B1_FILIAL = '00' and B1R.B1_COD = G1.G1_COD
+	from PROTHEUS_DB.dbo.SG1010 as G1 with(nolock)
+	inner join PROTHEUS_DB.dbo.SB1010 as B1R with(nolock) on B1R.D_E_L_E_T_ = '' and B1R.B1_FILIAL = '00' and B1R.B1_COD = G1.G1_COD
 	cross apply (select case when B1R.B1_QB = 0 then 1 else B1R.B1_QB end as qtd_base) as produto_pai
 	cross apply (
 		select codigo_superior, codigo, quantidade, perda, nivel, intermediario, ordem_hierarquica
 		from estoque.estrutura_com_recursividade(G1.G1_COMP, (G1.G1_QUANT / produto_pai.qtd_base) * @quantidade_base, @nivel, @mostra_intermediarios)
 	) as filhos
 	where G1.D_E_L_E_T_ = '' and G1_FILIAL = '00'
-	and (select count(*) from DADOSADV.dbo.SG1010 as G1I with(nolock) where G1I.D_E_L_E_T_ = '' and G1I.G1_FILIAL = '00' and G1I.G1_COD = G1.G1_COMP) > 0
+	and (select count(*) from PROTHEUS_DB.dbo.SG1010 as G1I with(nolock) where G1I.D_E_L_E_T_ = '' and G1I.G1_FILIAL = '00' and G1I.G1_COD = G1.G1_COMP) > 0
 	and G1_COD = @codigo_produto;
 
 	return;
